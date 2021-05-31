d849a98b8f998d1a98562830ac8ed3cf2e2b0350
107590022 <t107590022@ntut.org.tw>
2021-05-31 19:57:20 +0800

    clean Server

--- server/server.py
+++ server/server.py
@@ -1,48 +1,29 @@
 import socket
 import threading
 import json
-import base64
-import rsa
 import struct
-from rsa import PublicKey
 from RSA.RSA_main import RSA_main
-from RSA.RSA_Algrithm import RSA_Algrithm
 
 
 class Server:
-    """
-    服务器类
-    """
-
     def __init__(self):
-        """
-        构造
-        """
         self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__connections = list()
         self.__nicknames = list()
         self.__users_pub_keys = list()
 
     def __user_thread(self, user_id):
-        """
-        用户子线程
-        :param user_id: 用户id
-        """
         connection = self.__connections[user_id]
         nickname = self.__nicknames[user_id]
 
-        print('[Server] 用户', user_id, nickname, '加入聊天室')
+        print('[Server] user', user_id, nickname, '加入聊天室')
         self.__broadcast(message='Usre ' + str(nickname) +
                          '(' + str(user_id) + ')' + ' join')
 
-        # 侦听
         while True:
-            # noinspection PyBroadException
             try:
                 buffer = self.__recv_one_message(connection).decode()
-                # 解析成json数据
                 obj = json.loads(buffer)
-                # 如果是广播指令
                 if obj['type'] == 'broadcast':
                     self.__broadcast(obj['sender_id'], obj['message'])
                 elif obj['type'] == 'sendMessage':
@@ -56,9 +37,9 @@ class Server:
                     self.__send_one_message(
                         self.__connections[obj['receiver_id']], jsonByte)
                 elif obj['type'] == 'logout':
-                    print('[Server] 用户', user_id, nickname, '退出聊天室')
+                    print('[Server] user', user_id, nickname, '退出聊天室')
                     self.__broadcast(
-                        message='User ' + str(nickname) + '(' + str(user_id) + ')' + ' exeit')
+                        message='User ' + str(nickname) + '(' + str(user_id) + ')' + 'exeit')
                     self.__connections[user_id].close()
                     self.__connections[user_id] = None
                     self.__nicknames[user_id] = None
@@ -73,10 +54,10 @@ class Server:
                                 self.__connections[i], jsonByte)
                     break
                 else:
-                    print('[Server] 无法解析json数据包:',
+                    print('[Server] 無法解析json資料:',
                           connection.getsockname(), connection.fileno())
             except Exception:
-                print('[Server] 连接失效:', connection.getsockname(),
+                print('[Server] 連接失效:', connection.getsockname(),
                       connection.fileno())
                 self.__connections[user_id].close()
                 self.__connections[user_id] = None
@@ -106,14 +87,6 @@ class Server:
         length, = struct.unpack('!I', lengthbuf)
         return self.__recvall(sock, length)
 
-    # def __encryptMessage(self, receiver_id, message):
-    #     n, e = self.__users_pub_keys[receiver_id][0], self.__users_pub_keys[receiver_id][1]
-    #     message = message.encode('UTF-8')
-    #     message = rsa.encrypt(message, PublicKey(n, e))
-    #     message = base64.b64encode(message)
-    #     message = message.decode('UTF-8')
-    #     return message
-
     def __encryptMessage(self, receiver_id, message):
         publicKey = self.__users_pub_keys[receiver_id]
         message = message.encode('UTF-8')
@@ -121,14 +94,6 @@ class Server:
         return message
 
     def __sendMessageTo(self, sender_id=0, receiver_id=0, message=''):
-        """
-        广播
-        :param user_id: 用户id(0为系统)
-        :param message: 广播内容
-        """
-        #print("sender_id = " + str(sender_id))
-        #print("receiver_id = " + str(receiver_id))
-        # print(message)
         if (self.__connections[receiver_id] != None and sender_id != receiver_id):
 
             if (sender_id == 0):
@@ -143,19 +108,10 @@ class Server:
 
             self.__send_one_message(self.__connections[receiver_id], jsonByte)
 
-    def __turnPubKeyToList(self, pubKey):
-        n = pubKey['n']
-        e = pubKey['e']
-        return [n, e]
-
     def __waitForLogin(self, connection):
-        # 尝试接受数据
-        # noinspection PyBroadException
         try:
             buffer = self.__recv_one_message(connection).decode()
-            # 解析成json数据
             obj = json.loads(buffer)
-            # 如果是连接指令，那么则返回一个新的用户编号，接收用户连接
             if obj['type'] == 'login':
                 self.__connections.append(connection)
                 self.__nicknames.append(obj['nickname'])
@@ -175,29 +131,23 @@ class Server:
 
                         self.__send_one_message(userConnection, jsonByte)
 
-                # 开辟一个新的线程
                 thread = threading.Thread(
                     target=self.__user_thread, args=(len(self.__connections) - 1,))
                 thread.setDaemon(True)
                 thread.start()
             else:
-                print('[Server] 无法解析json数据包:',
+                print('[Server] 無法解析json數據包:',
                       connection.getsockname(), connection.fileno())
         except Exception:
-            print('[Server] 无法接受数据:', connection.getsockname(),
+            print('[Server] 無法接受数據:', connection.getsockname(),
                   connection.fileno())
 
     def start(self):
-        """
-        启动服务器
-        """
-        # 绑定端口
         self.__socket.bind(('127.0.0.1', 8888))
-        # 启用监听
-        self.__socket.listen(10)
-        print('[Server] 服务器正在运行......')
 
-        # 清空连接
+        self.__socket.listen(10)
+        print('[Server] 伺服器正在運行......')
+
         self.__users_pub_keys.clear()
         self.__connections.clear()
         self.__nicknames.clear()
@@ -205,10 +155,10 @@ class Server:
         self.__connections.append(None)
         self.__nicknames.append('System')
         self.__users_pub_keys.append(None)
-        # 开始侦听
+
         while True:
             connection, address = self.__socket.accept()
-            print('[Server] 收到一个新连接', connection.getsockname(),
+            print('[Server] 收到一个新連接', connection.getsockname(),
                   connection.fileno())
 
             thread = threading.Thread(
